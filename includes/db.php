66444542f859c32ea92f17a9f19957b635d29c7d
Linux here linux <165821171+mijsys@users.noreply.github.com>
2026-05-07 11:20:27 +0000

    chore: start 1.1.8

--- includes/db.php
+++ includes/db.php
@@ -380,7 +380,7 @@ function cms_init_db(PDO $db): void
         ['site_name', 'My CMS'],
         ['site_tagline', 'Nowy system CMS oparty o portfolio'],
         ['theme', 'default'],
-        ['cms_core_version', '1.1.7.8'],
+        ['cms_core_version', '1.1.8'],
         ['site_mode', 'multipage'],
         ['theme_variant', 'multipage'],
         ['site_default_language', 'pl'],
